08e8051a3c35ad039d43134999fea5c9b12deb2b
Leo Dahl <65565028+LeoDahl@users.noreply.github.com>
2025-02-10 23:46:23 +0100

    Update Calibrate.py

--- Calibrate.py
+++ Calibrate.py
@@ -1,6 +1,9 @@
-## Code made by Leo Dahl
-## Latest Change was made 2025-02-07 22:21
-
+##  File: Calibrate.py
+##  Author: Leo Dahl
+## Date: 2025-02-07
+##  Simple command prompter which runs commands via RunCommand() function
+##  The code listens for Serial information and moves two stepper motors
+##
 
 from SerialConnector import RunCommand
 
